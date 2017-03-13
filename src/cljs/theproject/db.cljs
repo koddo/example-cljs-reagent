d53c64b317a32bac4f88f0da9e28f268d1aaf1f6
Alex Scherbanov <alex@egotv.ru>
2017-03-13 13:28:22 +0300

    make it init app state with data from localstorage and sync it on localstorage update event

--- src/cljs/theproject/db.cljs
+++ src/cljs/theproject/db.cljs
@@ -7,15 +7,16 @@
 (s/def ::name string?)
 (s/def ::active-panel #{:home-panel :about-panel})
 (s/def ::pos (s/and number? #(>= % -1)))   ; it can be before the first beat
+(s/def ::moves (s/coll-of number? :kind vector? :distinct true))
 
-(s/def ::good-state (only-keys :req-un [::name ::active-panel ::pos]))
+(s/def ::good-state (only-keys :req-un [::name ::active-panel ::pos ::moves]))
 
 
 (def default-db
   {:name "re-frame"
    :active-panel :home-panel
    :pos 0
-
+   :moves []
    ;; :whatever true
    })
 
--- src/cljs/theproject/events.cljs
+++ src/cljs/theproject/events.cljs
@@ -10,11 +10,38 @@
 (def check-spec-interceptor
   (re-frame/after #(s/assert :theproject.db/good-state %)))
 
-(re-frame/reg-event-db
+;; (re-frame/reg-event-db
+;;  :initialize-db
+;;  [check-spec-interceptor re-frame/debug]
+;;  (fn  [_ _]
+;;    db/default-db))
+
+(re-frame/reg-cofx
+ :local-store 
+ (fn [coeffects local-store-key]
+   (assoc coeffects 
+          :local-store
+          (.getItem js/localStorage local-store-key))))
+
+(re-frame/reg-event-fx
  :initialize-db
+ [check-spec-interceptor re-frame/debug (re-frame/inject-cofx :local-store "rueda-moves-array")]
+ (fn [cofx _]
+   (let [moves (:local-store cofx)]
+     (if (nil? moves)
+       {:db db/default-db}
+       {:db (assoc db/default-db
+                   :moves (js->clj (js/JSON.parse moves)))}))))
+
+(.addEventListener js/window "storage"
+                   #(re-frame/dispatch [:storage-update %]))
+
+(re-frame/reg-event-db
+ :storage-update
  [check-spec-interceptor re-frame/debug]
- (fn  [_ _]
-   db/default-db))
+ (fn [db [_ storage-event]]
+   (println (.-key storage-event) (.-newValue storage-event))
+   (assoc db :moves (js->clj (js/JSON.parse (.getItem js/localStorage "rueda-moves-array"))))))
 
 (re-frame/reg-event-db
  :set-active-panel
@@ -29,7 +56,6 @@
   ]
  (fn [db [_ pos]]
    (assoc db :pos pos)))
-
 
 ;; )   ; end of trace-forms
 
